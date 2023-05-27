514c6a3bd3ffc3f73edee4890e881429e74bc0e1
LazyBoss07 <sec21it108@sairamtap.edu.in>
2023-05-27 12:49:28 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import com.example.library.LibraryClass;
+
+public class Main {
+    public static void main(String[] args) {
+        LibraryClass library = new LibraryClass();
+        library.displayMessage();
+    }
+}
